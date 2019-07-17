a8907dae020ade13c3382603f592021831814a3e
Christopher Fenner <Christopher.Fenner@sap.com>
2019-07-17 10:33:28 +0200

    move test script to test folder

--- test/integration/runTests.sh
+++ test/integration/runTests.sh
@@ -23,14 +23,21 @@ docker push localhost:5000/ppiper/neo-cli:latest
 git clone https://github.com/piper-validation/cloud-s4-sdk-book -b validate-neo-cli test-project
 pushd test-project
 
-docker run -v //var/run/docker.sock:/var/run/docker.sock -v $(pwd):/workspace -v /tmp\
-  -e CX_INFRA_IT_CF_PASSWORD -e CX_INFRA_IT_CF_USERNAME -e BRANCH_NAME=master \
-  -e CASC_JENKINS_CONFIG=/workspace/jenkins.yml -e HOST=$(hostname) \
+docker run \
+  -v //var/run/docker.sock:/var/run/docker.sock \
+  -v "$(pwd):/workspace" \
+  -v /tmp\
+  -e CX_INFRA_IT_CF_PASSWORD \
+  -e CX_INFRA_IT_CF_USERNAME \
+  -e BRANCH_NAME=master \
+  -e CASC_JENKINS_CONFIG=/workspace/jenkins.yml \
+  -e HOST=$(hostname) \
   ppiper/jenkinsfile-runner
 
 popd
 
 function cleanup {
   rm -rf test-project
+  docker rm --force registry
 }
 trap cleanup EXIT
